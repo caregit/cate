89527a5983f1de234de0a9a8460c5d9fa9224ce0
caregit <729340915@qq.com>
2020-04-03 16:44:14 +0800

    提交测试

---
+++ blog/app/Http/Controllers/UserController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 86158
+ * Date: 2020/4/3
+ * Time: 16:27
+ */
+
+namespace App\Http\Controllers;
+
+
+class UserController extends  Controller
+{
+
+    public function index(){
+        //dd(123456);
+        $test = "测试下这个结果级别是否正确";
+        return view("user.index",["test"=>$test]);
+    }
+}
