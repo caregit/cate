071c861e354c2f2e43f1d320cd212cd71c4cad91
caregit <729340915@qq.com>
2020-04-08 21:43:01 +0800

    test

--- blog/app/Http/Controllers/UserController.php
+++ blog/app/Http/Controllers/UserController.php
@@ -15,7 +15,7 @@ class UserController extends  Controller
 
     public function index(){
         //dd(123456);
-        $users = DB::table('users')->get();
+        $users = DB::table('users')->value('email');
         dd($users);
         $test = "测试下这个结果级别是否正确";
         return view("user.index",["test"=>$test]);
